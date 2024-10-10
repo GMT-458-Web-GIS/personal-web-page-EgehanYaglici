43090d0d8d611f733c1e10357cddb97c89fe705b
EgehanYaglici <egehanyaglici007@gmail.com>
2024-10-10 17:38:36 +0300

    Added Mail and LinkedIn Animations
    These animations made from scratch, can be easily use in another pages too.

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ function toggleTheme() {
   const newTheme = currentTheme === "light" ? "dark" : "light";
   body.setAttribute("data-theme", newTheme);
 }
+
+
+// LinkedIn animasyonu için metin değişimi kontrolü
+
+// LinkedIn Bağlantısını ve Yazı Elemanını Seçme
+const linkedinLink = document.querySelector('.linkedin-link');
+const linkedinText = document.getElementById('linkedin-text');
+
+// Hover Durumunda 'My Profile' Yazısı Göster ve Boşluğu Koru
+linkedinLink.addEventListener('mouseenter', () => {
+  linkedinText.innerHTML = '<span class="space">&nbsp;</span>My Profile';
+});
+
+// Hover Durumundan Çıkıldığında Boşluklu 'LinkedIn' Yazısına Geri Dön
+linkedinLink.addEventListener('mouseleave', () => {
+  linkedinText.innerHTML = '<span class="space">&nbsp;</span>LinkedIn';
+});
